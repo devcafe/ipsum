233d291acc46689dc823850db782b38036590c03
devcafe <cafe@2413>
2014-08-13 17:55:57 -0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	main/includes/header.php

--- main/mod_operacional/ajax/carregaDadosLoja.php
+++ main/mod_operacional/ajax/carregaDadosLoja.php
@@ -4,7 +4,7 @@
 	$idLoja = $_POST['idLoja'];
 
 	//Carrega dados
-	$sql = $pdo->prepare("Select * From ipsum_operacionallojas Where idLoja = ?");
+	$sql = $pdo->prepare("Select * From ipsum_operacionallojas a Inner Join ipsum_operacionalbandeiras b On (a.idEstabBandeira = b.idBandeira) Where idLoja = ?");
 
 	$sql->execute(array($idLoja));
 
