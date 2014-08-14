c6d6e49bfef7340429567c57f6ccf892b8f8789f
devcafe <helpdesk@cafecomunicacao.com.br>
2014-08-14 09:40:05 -0300

    V1.0

--- conf/conn.php
+++ conf/conn.php
@@ -1,5 +1,6 @@
 <?php
-	//$pdo = new PDO("mysql:host=localhost;dbname=ipsum", "root", "",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") );
+	$pdo = new PDO("mysql:host=localhost;dbname=ipsum", "root", "",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") );
 
-	$protheus = new PDO("mssql:host=192.168.0.9;dbname=DADOSP11", "sa", "c@f3@admin#");
+	//$protheus = new PDO("mssql:host=192.168.0.9;dbname=DADOSP11", "sa", "c@f3@admin#");
+	$protheus = new PDO("sqlsrv:Server=192.168.0.88\MSSQLSERVER,1433;Database=DADOSP11", "sa", "c@f3@admin#");
 ?>
