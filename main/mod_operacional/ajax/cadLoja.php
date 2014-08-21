727bd4e62a812fa84f55bbd44bb946294b959080
devcafe <cafe@2413>
2014-08-21 11:51:04 -0300

    v1.0

--- main/mod_operacional/ajax/cadLoja.php
+++ main/mod_operacional/ajax/cadLoja.php
@@ -13,7 +13,15 @@
 		$str = strtoupper($str);
 		return $str;
 	}
+	// validar campos
 
+	if ($cnpj == ""){
+
+	} elseif($idBandeiraHidden == ""){
+
+	} elseif($cep == "") $bairro == "" or $rua == "" or $ciade == "" or $uf == ""){
+		
+	}else{
 	//Insere chamado
 	$sql = $pdo->prepare("	Insert into `ipsum_operacionallojas` 
 								(
@@ -48,7 +56,7 @@
 								(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ");
 
 	$sql->execute(array('', $cnpj, $idBandeiraHidden, u($nome), u($rua), $numero, u($complemento), u($bairro), u($cidade), u($uf), $cep, u($estabReceitaAberturaData), u($estabReceitaRazaoSocial), u($estabReceitaNF), u($estabReceitaEndereco), $estabReceitaNumero, u($estabReceitaComplemento), u($estabReceitaBairro), u($estabReceitaCidade), u($estabReceitaUF), $estabReceitaCEP, u($estabReceitaSituacao), $estabReceitaSituacaoData, $estabTel01, $estabTel02, $dataFechamento, $userAdd));
-	
+	}
 	//Fecha conexão
 	$pdo = null;
 
