86d099c4a8e6dc7ee4dfd49e00c677aef040dcf4
devcafe <helpdesk@cafecomunicacao.com.br>
2014-09-03 14:07:52 -0300

    v1.0

--- main/mod_ti/ajax/carregaTabela.php
+++ main/mod_ti/ajax/carregaTabela.php
@@ -71,11 +71,48 @@
             $lista .= '<td> R$  '. number_format($total->Nov, 2, ',', '.') .'</td>';
             $lista .= '<td> R$  '. number_format($total->Dez, 2, ',', '.') .'</td>';
         $lista .= '</tr>';
-           
+
         $lista .= '<tr class = "subitens" id = "subItens_'. $resContaContabil->idContaContabil .'">';
            
         $lista .= '</tr>';
     }
+
+    //Total geral
+    $despesasGeral = $pdo->prepare("
+       Select
+            Sum(replace(jan, 'R$ ', '')) As Jan,
+            Sum(replace(fev, 'R$ ', '')) As Fev,
+            Sum(replace(mar, 'R$ ', '')) As Mar,
+            Sum(replace(abr, 'R$ ', '')) As Abr,
+            Sum(replace(mai, 'R$ ', '')) As Mai,
+            Sum(replace(jun, 'R$ ', '')) As Jun,
+            Sum(replace(jul, 'R$ ', '')) As Jul,
+            Sum(replace(ago, 'R$ ', '')) As Ago,
+            Sum(replace(sete, 'R$ ', '')) As Sete,
+            Sum(replace(outu, 'R$ ', '')) As Outu,
+            Sum(replace(nov, 'R$ ', '')) As Nov,
+            Sum(replace(dez, 'R$ ', '')) As Dez
+        From 
+            ipsum_tidespesas");
+
+    $despesasGeral->execute();
+    $totalGeral = $despesasGeral->fetch(PDO::FETCH_OBJ);
+
+    $lista .= '<tr>';
+        $lista .= '<td> <b> Total </b></td>';
+        $lista .= '<td><b> R$  '. number_format($totalGeral->Jan, 2, ',', '.') .'</b></td>';
+        $lista .= '<td><b> R$  '. number_format($totalGeral->Fev, 2, ',', '.') .'</b></td>';
+        $lista .= '<td><b> R$  '. number_format($totalGeral->Mar, 2, ',', '.') .'</b></td>';
+        $lista .= '<td><b> R$  '. number_format($totalGeral->Abr, 2, ',', '.') .'</b></td>';
+        $lista .= '<td><b> R$  '. number_format($totalGeral->Mai, 2, ',', '.') .'</b></td>';
+        $lista .= '<td><b> R$  '. number_format($totalGeral->Jun, 2, ',', '.') .'</b></td>';
+        $lista .= '<td><b> R$  '. number_format($totalGeral->Jul, 2, ',', '.') .'</b></td>';
+        $lista .= '<td><b> R$  '. number_format($totalGeral->Ago, 2, ',', '.') .'</b></td>';
+        $lista .= '<td><b> R$  '. number_format($totalGeral->Sete, 2, ',', '.') .'</b></td>';
+        $lista .= '<td><b> R$  '. number_format($totalGeral->Outu, 2, ',', '.') .'</b></td>';
+        $lista .= '<td><b> R$  '. number_format($totalGeral->Nov, 2, ',', '.') .'</b></td>';
+        $lista .= '<td><b> R$  '. number_format($totalGeral->Dez, 2, ',', '.') .'</b></td>';
+    $lista .= '</tr>';     
 
     echo $lista;
 
